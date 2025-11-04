300c384e073c11c2d0ca99ba76a017750565abf7
sergejtukavkin15-sketch <sergejtukavkin15@gmail.com>
2025-11-04 17:43:59 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,28 @@
 ﻿using System;
+using _4._11;
+using _4._11._4._11;
 
-class StringComparisonDemo
+namespace MyConsoleApp
 {
-    static void Main()
+    class Program
     {
-        string str1 = "Hello";
-        string str2 = "hello";
-        string str3 = "Hello";
-        string str4 = "World";
-        string text = "C# Programming Language";
+        static void Main(string[] args)
+        {
+            _4._11._4._11.Calculator calc = new Calculator();
 
-        Console.WriteLine("=== Демонстрация сравнения строк ===\n");
+           
+            calc.InitializeOperations();
+            calc.ShowMessage("Это тестовое сообщение");
 
+            
+            int sum = calc.doble(5, 7);
+            Console.WriteLine($"Сумма: {sum}");
 
-        Console.WriteLine($"1. Compare(\"{str1}\", \"{str2}\"): {string.Compare(str1, str2)}");
-        Console.WriteLine($"   Compare(\"{str1}\", \"{str3}\"): {string.Compare(str1, str3)}");
-        Console.WriteLine($"   Compare(\"{str1}\", \"{str4}\"): {string.Compare(str1, str4)}");
+            double product = calc.Division(3.5, 2.0);
+            Console.WriteLine($"Деление: {product}");
 
-
-        Console.WriteLine($"\n2. CompareOrdinal(\"{str1}\", \"{str2}\"): {string.CompareOrdinal(str1, str2)}");
-        Console.WriteLine($"   CompareOrdinal(\"{str1}\", \"{str1}\"): {string.CompareOrdinal(str1, str1)}");
-
-
-        Console.WriteLine($"\n3. Equals(\"{str1}\", \"{str2}\"): {str1.Equals(str2)}");
-        Console.WriteLine($"   Equals(\"{str1}\", \"{str3}\"): {str1.Equals(str3)}");
-        Console.WriteLine($"   string.Equals(\"{str1}\", \"{str3}\"): {string.Equals(str1, str3)}");
-
-
-        Console.WriteLine($"\n4. \"{str1}\" == \"{str2}\": {str1 == str2}");
-        Console.WriteLine($"   \"{str1}\" == \"{str3}\": {str1 == str3}");
-        Console.WriteLine($"   \"{str1}\" != \"{str4}\": {str1 != str4}");
-
-
-        Console.WriteLine($"\n5. IndexOf в \"{text}\":");
-        Console.WriteLine($"   IndexOf('P'): {text.IndexOf('P')}");
-        Console.WriteLine($"   IndexOf(\"Prog\"): {text.IndexOf("Prog")}");
-        Console.WriteLine($"   IndexOf('z'): {text.IndexOf('z')}");
-
-
-        Console.WriteLine($"\n6. Contains в \"{text}\":");
-        Console.WriteLine($"   Contains(\"Programming\"): {text.Contains("Programming")}");
-        Console.WriteLine($"   Contains(\"Java\"): {text.Contains("Java")}");
+            string greeting = calc.GetGreeting("Андрей");
+            Console.WriteLine(greeting);
+        }
     }
-
-       
 }
