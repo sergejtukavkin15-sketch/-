09c64cbb78612d8f97ead5cbd99febfdf3f2e1b8
sergejtukavkin15-sketch <sergejtukavkin15@gmail.com>
2025-11-04 17:30:33 +0900

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _4._11
+{
+    using System;
+
+    namespace _4._11
+    {
+        public class Calculator
+        {
+            public void InitializeOperations()
+            {
+                Console.WriteLine(3+4);
+            }
+            public void ShowMessage(string message)
+            {
+                Console.WriteLine($"Сообщение: {message}");
+            }
+
+
+            public int doble(int a, int b)
+            {
+                return a + b;
+            }
+            public double Division(double x, double y)
+            {
+                return x / y;
+            }
+            public string GetGreeting(string name)
+            {
+                return $"Здравствуйте, {name}!";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using _4._11;
+using _4._11._4._11;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _4._11._4._11.Calculator calc = new Calculator();
+
+           
+            calc.InitializeOperations();
+            calc.ShowMessage("Это тестовое сообщение");
+
+            
+            int sum = calc.doble(5, 7);
+            Console.WriteLine($"Сумма: {sum}");
+
+            double product = calc.Division(3.5, 2.0);
+            Console.WriteLine($"Деление: {product}");
+
+            string greeting = calc.GetGreeting("Андрей");
+            Console.WriteLine(greeting);
+        }
+    }
+}
